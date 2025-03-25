fc0d1c0d3d1f9a5b6873df4f9c5af43e0ebf955c
Breno Fortes <brenofortes.f1@hotmail.com>
2025-03-25 09:08:07 -0300

    aula-4.1

--- Lista2/ex1/TestePessoa.java
+++ Lista2/ex1/TestePessoa.java
@@ -6,5 +6,9 @@ public class TestePessoa {
     p1.nome = "Breno";
     p1.telefone = "3333-4444";
     p1.exibeDados();
+    Pessoa p2 = new Pessoa();
+    p2.nome = "Carlos";
+    p2.telefone = "2222-3333";
+    p2.exibeDados();
   }
 }
---
+++ Lista2/ex2/Universidade.java
@@ -0,0 +1,10 @@
+package ex2;
+
+public class Universidade {
+  String nome, cidade, estado;
+
+  public void exibeDados() {
+    System.out.println("nome: " + nome);
+    System.out.println("cidade: " + cidade + "-" + estado);
+  }
+}
